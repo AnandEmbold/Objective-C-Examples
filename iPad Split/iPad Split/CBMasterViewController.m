dbfa1cf105847f765c27684bd7d33f1cf63142b1
Carlos Butron <carlosbutron@icloud.com>
2015-09-02 16:37:31 +0200

    Updated. Translated to English. Fixed some bugs.

--- iPad Split/iPad Split/CBMasterViewController.m
+++ iPad Split/iPad Split/CBMasterViewController.m
@@ -169,6 +169,8 @@
         case NSFetchedResultsChangeDelete:
             [self.tableView deleteSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationFade];
             break;
+        default:
+            break;
     }
 }
 

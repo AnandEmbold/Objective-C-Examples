2704dbfd9aa02500c56a6a5f7f0b439c547e8e0a
Carlos Butron <carlosbutron@icloud.com>
2015-09-03 06:31:38 +0200

    Updated. Translated to English. Fixed some bugs.

---
+++ Arkanoid Clon/PingPong/Brick.h
@@ -0,0 +1,17 @@
+//
+//  Raqueta.h
+//  PingPong
+//
+//  Created by Carlos Butron on 25/05/14.
+//
+
+#import "cocos2d.h"
+
+@interface Brick : CCSprite <CCTouchOneByOneDelegate>
+
+@property (nonatomic)           BOOL   caught;
+@property (nonatomic, readonly) CGRect rect;
+@property (nonatomic, readonly) CGRect rectInPixels;
+
+@end
+

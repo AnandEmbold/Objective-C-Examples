ac8628bb5812af0097ae6639b9e5f842365deb08
Carlos Butron <carlosbutron@icloud.com>
2015-09-04 02:51:54 +0200

    Updated. Translated to English. Fixed some bugs.

--- Board with Touch Events/Board with Touch Events/Board.m
+++ Board with Touch Events/Board with Touch Events/Board.m
@@ -1,83 +1,119 @@
 //
-//  CBPizarra.m
-//  EventosTouch
+//  Board.m
+//  Board with Touch Events
 //
 //  Created by Carlos on 06/04/14.
 //  Copyright (c) 2014 Carlos Butron. All rights reserved.
 //
 
 #import "Board.h"
-
+#import "Line.h"
 
 @interface Board(){
-    CGPoint currentPoint;
-    CGPoint previousPoint1;
-
+    CGPoint start;
+    CGPoint end;
+    NSMutableArray * _lines;
 }
+
+@property (readonly,getter = lines ) NSMutableArray * lines;
 
 @end
 
-
 @implementation Board
+@synthesize lines;
+
+-(IBAction)delete:(id)sender
+{
+    //Reiniciar la vista borrando todas las líneas.
+    [self.lines removeAllObjects];
+    
+    [self setNeedsDisplay];
+}
 
 - (id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
     if (self) {
-        // Initialization code
     }
     return self;
 }
 
+-(NSMutableArray *)lineas{
+    if (_lines == nil) {
+        _lines = [NSMutableArray new];
+    }
+    return _lines;
+}
 
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect
 {
-    // Drawing code
-    CGContextRef context = UIGraphicsGetCurrentContext();
     
-    UIColor * red = [UIColor redColor];
+    //Pintar todas las líneas de NSMutableArray lineas de color rojo y con un grosor de línea de 3.
     
-    CGContextMoveToPoint(context, previousPoint1.x, previousPoint1.y);
     
-    CGContextAddLineToPoint(context, currentPoint.x, currentPoint.y);
+    CGContextRef contexto = UIGraphicsGetCurrentContext();
     
-    CGContextSetLineCap(context, kCGLineCapRound);
+    UIColor * rojo = [UIColor redColor];
     
-    CGContextSetLineWidth(context, 3);
+    CGContextSetStrokeColorWithColor(contexto, rojo.CGColor);
     
-    CGContextSetStrokeColorWithColor(context, red.CGColor);
-    
-    CGContextStrokePath(context);
+    CGContextSetLineWidth(contexto, 3);
     
     
     
- 
+    
+    for (int i=0; i<_lines.count; i++) {
+        Line * line = [self.lineas objectAtIndex:i];
+        //NSLog(@"drawrect  %f, %f, %f, %f", linea.puntos.origin.x, linea.puntos.origin.y, linea.puntos.size.width, linea.puntos.size.height);
+        
+        CGContextMoveToPoint(contexto, line.points.origin.x, line.points.origin.y);
+        CGContextAddLineToPoint(contexto, line.points.size.width, line.points.size.height);
+        CGContextStrokePath(contexto);
+        
+        
+    }
+    
+    
+    
+    CGContextStrokePath(contexto);
+    
+    
+    
     
 }
 
 
-- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
     
+    //Actualizar los atributos inicio y fin.
     UITouch *touch = [touches anyObject];
-    previousPoint1 = [touch previousLocationInView:self];
-    currentPoint = [touch locationInView:self];
+    start = [touch previousLocationInView:self];
+    end = [touch locationInView:self];
+    
     [self setNeedsDisplay];
-
+    
 }
 
 -(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event{
     
-    UITouch *touch  = [touches anyObject];
-
-    previousPoint1  = [touch previousLocationInView:self];
-
-    currentPoint    = [touch locationInView:self];
-
-[self setNeedsDisplay];
-
+    //Añadir las coordenadas del movimiento al NSMutableArray lineas haciendo uso de la clase GTLinea.
+    //Actualizar los atributos inicio y fin.
+    //Recargar la vista.
+    
+    UITouch * touch = [touches anyObject];
+    start = [touch previousLocationInView:self];
+    end = [touch locationInView:self];
+    Line * line = [[Line alloc] initWithRect:CGRectMake(start.x, start.y, end.x, end.y)];
+    
+    // NSLog(@"touchesmoved   %f, %f, %f, %f", inicio.x, inicio.y, fin.x, fin.y);
+    
+    [self.lineas addObject: line];
+    start.x = end.x;
+    start.y = end.y;
+    
+    [self setNeedsDisplay];
     
 }
+
 
 @end
--- Board with Touch Events/Board with Touch Events/Line.h
+++ Board with Touch Events/Board with Touch Events/Line.h
@@ -1,13 +1,17 @@
 //
-//  Board.h
+//  Line.h
 //  Board with Touch Events
 //
 //  Created by Carlos on 06/04/14.
 //  Copyright (c) 2014 Carlos Butron. All rights reserved.
 //
-
 #import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
-@interface Board : UIView
+@interface Line : NSObject
+
+@property (readwrite) CGRect points;
+
+-(id) initWithRect:(CGRect) points;
 
 @end

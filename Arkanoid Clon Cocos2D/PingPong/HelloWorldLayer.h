1331f276b67ba1fd7741b6d71d80097afc11f9c4
Carlos Butron <beiosdev@icloud.com>
2016-03-15 10:07:19 +0100

    Updated. Translated to English. Fixed some bugs.

--- Arkanoid Clon Cocos2D/PingPong/HelloWorldLayer.h
+++ Arkanoid Clon Cocos2D/PingPong/HelloWorldLayer.h
@@ -5,15 +5,13 @@
 //  Created by Carlos Butron on 25/05/14.
 //
 
-
 #import <GameKit/GameKit.h>
 
 // When you import this file, you import all the cocos2d classes
 #import "cocos2d.h"
 
 // HelloWorldLayer
-@interface HelloWorldLayer : CCLayer 
-{
+@interface HelloWorldLayer : CCLayer {
 }
 
 // returns a CCScene that contains the HelloWorldLayer as the only child

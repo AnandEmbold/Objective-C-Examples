cd01f4f9b23adac46dba09a6f86a528ff318c8ed
Carlos Butron <beiosdev@icloud.com>
2016-04-13 10:27:42 +0200

    Updated. Translated to English. Fixed some bugs.

--- CoreLocation/CoreLocationApp/MainViewController.m
+++ CoreLocation/CoreLocationApp/MainViewController.m
@@ -9,7 +9,7 @@
 #import "MainViewController.h"
 @import CoreLocation;
 
-@interface MainViewController ()<CLLocationManagerDelegate>
+@interface MainViewController () <CLLocationManagerDelegate>
 
 @property (nonatomic, strong) CLLocationManager *locationManager;
 @property (weak, nonatomic) IBOutlet UILabel *latitudeLabel;
@@ -22,34 +22,28 @@
 
 @implementation MainViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad 
+{
     [super viewDidLoad];
     
     [self locationManagerStart];
-    
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
 
-- (void)locationManagerStart {
-    
+- (void)locationManagerStart 
+{
     //startup
-    
     self.locationManager = [[CLLocationManager alloc] init];
     
     //check if services enabled
-    if ([CLLocationManager locationServicesEnabled]){
+    if ([CLLocationManager locationServicesEnabled]) {
         //ask for permission
-        if ([self.locationManager respondsToSelector:@selector(requestWhenInUseAuthorization)]){
-            
+        if ([self.locationManager respondsToSelector:@selector(requestWhenInUseAuthorization)]) {
             [self.locationManager requestWhenInUseAuthorization];
             //add to plist :NSLocationWhenInUseUsageDescription
-            
         }
-        
     }
-    
-    
+
     //start service
     self.locationManager.delegate = self;
     
@@ -57,51 +51,38 @@
     [self.locationManager startUpdatingLocation];
     
     if ([CLLocationManager headingAvailable]) {
-        
         [self.locationManager startUpdatingHeading];
     }
-    
-    
-
-    
-    
 }
 
--(void)viewDidDisappear:(BOOL)animated {
-    
+- (void)viewDidDisappear:(BOOL)animated 
+{
     [self.locationManager stopUpdatingHeading];
     [self.locationManager stopUpdatingLocation];
-    
 }
 
 //implement delegate methods
--(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
-    
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations 
+{
     CLLocation *location = locations.lastObject;
     self.latitudeLabel.text = [NSString stringWithFormat:@"%f", location.coordinate.latitude];
     self.longitudeLabel.text = [NSString stringWithFormat:@"%f", location.coordinate.longitude];
     self.altitudeLabel.text = [NSString stringWithFormat:@"%.01f", location.altitude];
-    
 }
 
 
-- (void)locationManager:(CLLocationManager *)manager didUpdateHeading:(CLHeading *)newHeading {
-    
+- (void)locationManager:(CLLocationManager *)manager didUpdateHeading:(CLHeading *)newHeading 
+{
     self.headingLabel.text = [NSString stringWithFormat:@"%.01f", newHeading.trueHeading];
-    
     //Rotate 270 degrees the label text of Yorkie's gender (M_PI * 270 /-180) 90º
     //[yorkieMainCell.genderYorkieMainLabel setTransform:CGAffineTransformMakeRotation(M_PI / 2)];
     [self.arrowImageView setTransform:CGAffineTransformMakeRotation(newHeading.trueHeading * (M_PI / -180))];
-    
-    
 }
 
--(BOOL)locationManagerShouldDisplayHeadingCalibration:(CLLocationManager *)manager {
-    
+- (BOOL)locationManagerShouldDisplayHeadingCalibration:(CLLocationManager *)manager 
+{
     return YES;
-    
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];

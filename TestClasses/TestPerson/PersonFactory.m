c90686c84011d34af7b819edfd1b1d030b2aa188
Carlos Butron <beiosdev@icloud.com>
2016-04-21 09:53:36 +0200

    Updated. Translated to English. Fixed some bugs.

--- TestClasses/TestPerson/PersonFactory.m
+++ TestClasses/TestPerson/PersonFactory.m
@@ -10,26 +10,20 @@
 
 @implementation PersonFactory
 
-
-+ (Person *)createsPersonWithName:(NSString *)name surname:(NSString *)surname idNumber:(NSString *)idNumber{
-    
++ (Person *)createsPersonWithName:(NSString *)name surname:(NSString *)surname idNumber:(NSString *)idNumber
+{
     Person *person = [[Person alloc] initWithName:name surname:surname ID:idNumber];
-    
     return person;
-   
 }
 
-+ (Employee *)createsEmployeeWithName:(NSString *)name surname:(NSString *)surname idNumber:(NSString *)idNumber employeeId:(NSString *)employeeId{
-    
++ (Employee *)createsEmployeeWithName:(NSString *)name surname:(NSString *)surname idNumber:(NSString *)idNumber employeeId:(NSString *)employeeId
+{
     Employee *employee = [[Employee alloc] initWithName:name surname:surname ID:idNumber employeeID:employeeId];
-    
     return employee;
-    
 }
 
-+ (NSString *)interestingIdForPersonOrEmployee: (id)personOrEmployee{
-    
-    
++ (NSString *)interestingIdForPersonOrEmployee:(id)personOrEmployee
+{
     if ([personOrEmployee isMemberOfClass:[Employee class]]){
         //if its employee
 //        Employee *employee = (Employee *)personOrEmployee;  //this is another way to do the same stuff
@@ -43,30 +37,19 @@
     } else {
         return @"Not a valid ID";
     }
-
 }
 
-
-+ (NSArray *)arrayWithOnlyPersonsFromArray:(NSArray *)originalArray{
-    
-    
++ (NSArray *)arrayWithOnlyPersonsFromArray:(NSArray *)originalArray
+{
     NSMutableArray *result = [NSMutableArray arrayWithArray:originalArray];
-    
-    
-    for (id object in originalArray){
-        
+
+    for (id object in originalArray) {
         if (!([object isMemberOfClass:[Person class]])){
             [result removeObject:object];
         }
-        
     }
-    
-    
+
     return result;
 }
-
-    
-
-
 
 @end

1b2ea9a242b303ccee704b261a012ec65da61958
Carlos Butron <beiosdev@icloud.com>
2016-04-29 20:59:09 +0200

    Updated. Translated to English. Fixed some bugs.

--- TestPicker/TestPicker/ModalViewController.m
+++ TestPicker/TestPicker/ModalViewController.m
@@ -10,47 +10,26 @@
 
 @interface ModalViewController ()
 
-
-
 @end
 
 @implementation ModalViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad 
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning 
+{
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
-//one way to back
-//- (IBAction)prepareForUnwind:(UIStoryboardSegue *) sender{
-//    
-//    
-//}
-
-
-- (IBAction)didTapExitButton:(id)sender{
+- (IBAction)didTapExitButton:(id)sender
+{
     [self dismissViewControllerAnimated:YES completion:^{
         //do something with saving or whatever
-        
         //everything that goes here will get executed after the viewcontroller disappears
     }];
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
-
 
 @end

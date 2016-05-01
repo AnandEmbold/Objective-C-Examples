f002f2c9df8e8f4b295ff7d25d7dcb4bb546093e
Carlos Butron <beiosdev@icloud.com>
2016-05-01 14:40:25 +0200

    Updated. Translated to English. Fixed some bugs.

--- UICollectionView and UITableView/UICollectionView/CBAlbumViewController.m
+++ UICollectionView and UITableView/UICollectionView/CBAlbumViewController.m
@@ -15,10 +15,11 @@
 @end
 
 @implementation CBAlbumViewController
+
 @synthesize album = _album;
 
-- (NSMutableArray *) album{
-    
+- (NSMutableArray *)album
+{
     if(!_album){
         _album = [NSMutableArray new];
         for (int i=0; i<10; i++){
@@ -40,59 +41,38 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
-
-- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView{
-    
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
     return 1;
-    
 }
 
-- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
-    
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
     return self.album.count;
-    
 }
 
--(UICollectionViewCell*)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
-    
+- (UICollectionViewCell*)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
     CBCollectionCellPhoto * cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"CellFrame" forIndexPath:indexPath];
     
     cell.imgFile = [self.album objectAtIndex:indexPath.row];
     return cell;
-    
 }
 
--(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
-    
-    if ([segue.identifier isEqualToString:@"showPhoto"]){
-        
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if ([segue.identifier isEqualToString:@"showPhoto"]) {
         CBMarcoViewController * destino = segue.destinationViewController;
         NSIndexPath * indexPath = [[self.collectionView indexPathsForSelectedItems] objectAtIndex:0];
         destino.imageFile = [self.album objectAtIndex:indexPath.row];
-        
     }
-    
 }
-
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
